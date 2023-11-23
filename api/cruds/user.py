a7097a2739dc7fdd51f616bc102def5463624013
karintou8710 <karintou8710@gmail.com>
2023-11-23 03:31:25 +0000

    [add] hash password

--- api/cruds/user.py
+++ api/cruds/user.py
@@ -2,6 +2,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.models.user import User as UserModel
+from api.oauth2 import get_password_hash
 from api.schemas.user import SignUp
 
 
@@ -13,7 +14,7 @@ async def fetch_user_by_name(db: AsyncSession, name: str) -> UserModel | None:
 
 
 async def create_user(db: AsyncSession, sign_up: SignUp) -> UserModel:
-    user = UserModel(**sign_up.dict())
+    user = UserModel(name=sign_up.name, password=get_password_hash(sign_up.password))
     db.add(user)
     await db.flush()
     await db.refresh(user)
--- api/oauth2.py
+++ api/oauth2.py
@@ -2,10 +2,36 @@ from typing import Annotated
 
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.cruds import user as user_cruds
+from api.models.user import User as UserModel
 from api.types.oauth2 import JwtUser
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    isSame: bool = pwd_context.verify(plain_password, hashed_password)
+    return isSame
+
+
+def get_password_hash(password: str) -> str:
+    hashed_password: str = pwd_context.hash(password)
+    return hashed_password
+
+
+async def authenticate_user(
+    db: AsyncSession, username: str, password: str
+) -> UserModel | None:
+    user = await user_cruds.fetch_user_by_name(db, username)
+    if user is None:
+        return None
+    if not verify_password(password, user.password):
+        return None
+    return user
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> JwtUser:
