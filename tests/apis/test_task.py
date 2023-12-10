2d8a92d90686f7998f0608cb5acabad4e92c534b
karintou8710 <karintou8710@gmail.com>
2023-12-10 10:32:50 +0000

    [add] delete task test

--- tests/apis/test_task.py
+++ tests/apis/test_task.py
@@ -7,6 +7,7 @@ from api.cruds import task as task_cruds
 from api.schemas import task as task_schema
 from tests.data import (
     FORBIDDEN_ERROR_RESPONSE,
+    NO_TASK_ERROR_RESPONSE,
     NOT_AUTHENTICATED_ERROR_RESPONSE,
     VALIDATION_ERROR_RESPONSE,
 )
@@ -297,6 +298,23 @@ class TestUpdateTask:
         assert data == VALIDATION_ERROR_RESPONSE
 
     @pytest.mark.asyncio
+    async def test_error_no_task(
+        self,
+        async_client: AsyncClient,
+        user_signup: SignUpUserInfo,
+    ) -> None:
+        body: CreateTask = {"title": "notask"}
+        response = await async_client.put(
+            "/api/tasks/e3c114a3-48a9-48c3-9c9d-c50cbea02272",
+            json=body,
+            headers={"Authorization": f"Bearer {user_signup['accessToken']}"},
+        )
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        data: ErrorResponse = response.json()
+        assert data == NO_TASK_ERROR_RESPONSE
+
+    @pytest.mark.asyncio
     async def test_error_update_other_user_task(
         self,
         async_client: AsyncClient,
@@ -319,6 +337,105 @@ class TestUpdateTask:
         response = await async_client.put(
             f"/api/tasks/{task_id}",
             json=body,
+            headers={"Authorization": f"Bearer {user_signup2['accessToken']}"},
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+        data: ErrorResponse = response.json()
+        assert data == FORBIDDEN_ERROR_RESPONSE
+
+
+class TestDeleteTask:
+    @pytest.mark.asyncio
+    async def test_delete_task(
+        self,
+        async_client: AsyncClient,
+        user_signup: SignUpUserInfo,
+        test_db: AsyncSession,
+    ) -> None:
+        # user1でタスクを作成する
+        async with test_db.begin():
+            param: task_schema.CreateTask = task_schema.CreateTask(
+                title="University Assignments"
+            )
+            task = await task_cruds.create_task(
+                test_db, user_signup["user"]["id"], param
+            )
+            task_id = task.id
+
+        response = await async_client.delete(
+            f"/api/tasks/{task_id}",
+            headers={"Authorization": f"Bearer {user_signup['accessToken']}"},
+        )
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+        data: str = response.text
+        assert data == ""
+
+        async with test_db.begin():
+            res = await task_cruds.fetch_tasks_by_id(test_db, task_id)
+            assert res is None
+
+    @pytest.mark.asyncio
+    async def test_error_delete_task_without_jwt(
+        self,
+        async_client: AsyncClient,
+        user_signup: SignUpUserInfo,
+        test_db: AsyncSession,
+    ) -> None:
+        # user1でタスクを作成する
+        async with test_db.begin():
+            param: task_schema.CreateTask = task_schema.CreateTask(
+                title="University Assignments"
+            )
+            task = await task_cruds.create_task(
+                test_db, user_signup["user"]["id"], param
+            )
+            task_id = task.id
+        response = await async_client.delete(
+            f"/api/tasks/{task_id}",
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        data: ErrorResponse = response.json()
+        assert data == NOT_AUTHENTICATED_ERROR_RESPONSE
+
+    @pytest.mark.asyncio
+    async def test_error_no_task(
+        self,
+        async_client: AsyncClient,
+        user_signup: SignUpUserInfo,
+    ) -> None:
+        response = await async_client.delete(
+            "/api/tasks/e3c114a3-48a9-48c3-9c9d-c50cbea02272",
+            headers={"Authorization": f"Bearer {user_signup['accessToken']}"},
+        )
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+        data: ErrorResponse = response.json()
+        assert data == NO_TASK_ERROR_RESPONSE
+
+    @pytest.mark.asyncio
+    async def test_error_deletee_other_user_task(
+        self,
+        async_client: AsyncClient,
+        user_signup: SignUpUserInfo,
+        user_signup2: SignUpUserInfo,
+        test_db: AsyncSession,
+    ) -> None:
+        # user1でタスクを作成する
+        async with test_db.begin():
+            param: task_schema.CreateTask = task_schema.CreateTask(
+                title="University Assignments"
+            )
+            task = await task_cruds.create_task(
+                test_db, user_signup["user"]["id"], param
+            )
+            task_id = task.id
+
+        # user2で削除する
+        response = await async_client.delete(
+            f"/api/tasks/{task_id}",
             headers={"Authorization": f"Bearer {user_signup2['accessToken']}"},
         )
         assert response.status_code == status.HTTP_403_FORBIDDEN
