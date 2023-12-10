9f49bcd4312fc8dcc298328840ef49de75de3bee
karintou8710 <karintou8710@gmail.com>
2023-12-10 06:34:55 +0000

    [add] create task endpoint

--- api/routers/task.py
+++ api/routers/task.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.cruds import task as task_crud
@@ -11,9 +11,21 @@ router = APIRouter()
 
 
 @router.get("/users/me/tasks", response_model=list[task_schema.Task])
-async def read_me(
+async def read_my_tasks(
     current_user: UserModel = Depends(get_current_user),
     db: AsyncSession = Depends(get_db),
 ) -> list[task_schema.Task]:
     tasks = await task_crud.fetch_tasks_by_uid(db, current_user.id)
     return [task_schema.Task.model_validate(task) for task in tasks]
+
+
+@router.post(
+    "/tasks", response_model=task_schema.Task, status_code=status.HTTP_201_CREATED
+)
+async def create_task(
+    create_body: task_schema.CreateTask,
+    current_user: UserModel = Depends(get_current_user),
+    db: AsyncSession = Depends(get_db),
+) -> task_schema.Task:
+    task = await task_crud.create_task(db, current_user.id, create_body)
+    return task_schema.Task.model_validate(task)
